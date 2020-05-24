7b79f7495d3eceb3cf8b888343272a187dd6a156
Ldrrr <64193116+Ldrrr@users.noreply.github.com>
2020-05-24 21:55:01 +0800

    有效的完全平方数
    给定一个正整数 num，编写一个函数，如果 num 是一个完全平方数，则返回 True，否则返回 False。
    
    说明：不要使用任何内置的库函数，如  sqrt。

---
+++ 有效的完全平方数.c
@@ -0,0 +1,21 @@
+bool isPerfectSquare(int num)
+{
+    if(num==1) return true;
+    int left=0,right=num/2;
+    while(left<=right)
+    {
+        long mid=(left+right)/2;
+        if(mid*mid==num)
+        {
+            return true;
+        }
+        else if(mid*mid<num)
+        {
+            left=mid+1;
+        }
+        else{
+            right=mid-1;
+        }
+    }
+    return false;
+}
